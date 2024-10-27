2e154644b3945ce498c613090f14b6e0e8478440
rafael <rafaelcoelho2711@gmail.com>
2024-10-27 18:43:30 -0300

    integração com prisma orm

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Rafael Coelho',
+    email: 'rafael@email.com',
+    password: 'senha123',
+    profile: 'Deliverer',
+  },
+})
