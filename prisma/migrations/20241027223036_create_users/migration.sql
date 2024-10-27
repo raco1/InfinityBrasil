0937d4292dba57d69b9b6cd2d2d4845b86999b12
rafael <rafaelcoelho2711@gmail.com>
2024-10-27 19:34:53 -0300

    cria a primeira migration

---
+++ prisma/migrations/20241027223036_create_users/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Profile" AS ENUM ('Deliverer', 'Company');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profile" "Profile" NOT NULL DEFAULT 'Deliverer',
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
