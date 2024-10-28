690db43523170026befef2e9dd473034a3f4a8b3
rafael <rafaelcoelho2711@gmail.com>
2024-10-28 13:47:39 -0300

    Lidando com erros do use case

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,6 @@
 import { UserRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseServiceRequest {
   name: string
@@ -19,7 +20,7 @@ export class RegisterUseService {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('Email already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
