8314563e094941aaf841f2bb1c2253d303f711e7
rafael <rafaelcoelho2711@gmail.com>
2024-10-31 14:20:06 -0300

    altera o verify pois estava retornando undefined

--- src/http/middlewares/verify-user-profile.ts
+++ src/http/middlewares/verify-user-profile.ts
@@ -1,11 +1,17 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeGetUserProfileService } from '@/services/factory/make-get-user-profile-service'
 
 export function verifyUserProfile(profileToVerify: 'Company' | 'Deliverer') {
   return async (request: FastifyRequest, reply: FastifyReply) => {
-    const { profile } = request.user
+    const getUserProfile = makeGetUserProfileService()
+    const { user } = await getUserProfile.execute({
+      userId: request.user.sub,
+    })
 
-    if (profile === profileToVerify) {
+    if (user.profile !== profileToVerify) {
       return reply.status(401).send({ message: 'Unauthorized.' })
     }
   }
 }
+
+// Tentar entender o porquê antes estava retornando undefined
