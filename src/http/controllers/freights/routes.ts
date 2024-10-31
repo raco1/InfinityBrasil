41a4058c1e11b3f39f0e717c8b6e1542d47fa510
rafael <rafaelcoelho2711@gmail.com>
2024-10-31 14:17:55 -0300

    alteração no nome do arquivo

--- src/http/controllers/freights/routes.ts
+++ src/http/controllers/freights/routes.ts
@@ -3,6 +3,8 @@ import { FastifyInstance } from 'fastify'
 import { update } from './update'
 import { verifyUserProfile } from '@/http/middlewares/verify-user-profile'
 import { register } from './register'
+import { freights } from './freights'
+import { registerRequest } from '../requests/register'
 
 export async function freightsRoutes(app: FastifyInstance) {
   app.post(
@@ -10,5 +12,21 @@ export async function freightsRoutes(app: FastifyInstance) {
     { onRequest: [verifyJWT, verifyUserProfile('Company')] },
     register,
   )
+
+  app.post(
+    '/freight/:freight_id/request',
+    {
+      onRequest: [verifyJWT, verifyUserProfile('Deliverer')],
+    },
+    registerRequest,
+  )
   app.patch('/freight/update', update)
+
+  app.get(
+    '/freights',
+    {
+      onRequest: [verifyJWT, verifyUserProfile('Deliverer')],
+    },
+    freights,
+  )
 }
