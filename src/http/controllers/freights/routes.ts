2a5431b246712d36fa625f07f07198448649abce
rafael <rafaelcoelho2711@gmail.com>
2024-10-31 19:43:01 -0300

    refaz as rotas em seus deivdos locais

--- src/http/controllers/freights/routes.ts
+++ src/http/controllers/freights/routes.ts
@@ -1,11 +1,8 @@
-import { acceptDeliveryRequest } from '../requests/accepted-request'
 import { verifyUserProfile } from '@/http/middlewares/verify-user-profile'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
-import { registerRequest } from '../requests/register'
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { freights } from './freights'
-import { declineDeliveryRequest } from '../requests/declined-request'
 
 export async function freightsRoutes(app: FastifyInstance) {
   //* Registrar um novo frete (Somente empresas) */
@@ -22,28 +19,5 @@ export async function freightsRoutes(app: FastifyInstance) {
       onRequest: [verifyJWT, verifyUserProfile('Deliverer')],
     },
     freights,
-  )
-
-  //* Solicitar frete específico disponível (Somente entregadores) */
-  app.post(
-    '/freight/:freight_id/request',
-    {
-      onRequest: [verifyJWT, verifyUserProfile('Deliverer')],
-    },
-    registerRequest,
-  )
-
-  //* Atualizações de frete */
-  //* Aceitar solicitação */
-  app.patch(
-    '/freight/requests/:request_id/accept',
-    { onRequest: [verifyJWT, verifyUserProfile('Company')] },
-    acceptDeliveryRequest,
-  )
-  //* Rejeitar solicitação */
-  app.patch(
-    '/freight/requests/:request_id/decline',
-    { onRequest: [verifyJWT, verifyUserProfile('Company')] },
-    declineDeliveryRequest,
   )
 }
--- src/http/controllers/requests/routes.ts
+++ src/http/controllers/requests/routes.ts
@@ -1,13 +1,38 @@
-import { FastifyInstance } from 'fastify'
-
-import { requests } from './requests'
 import { verifyUserProfile } from '@/http/middlewares/verify-user-profile'
+import { declineDeliveryRequest } from './declined-request'
+import { acceptDeliveryRequest } from './accepted-request'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { registerRequest } from './register'
+import { FastifyInstance } from 'fastify'
+import { requests } from './requests'
 
 export async function requestsRoutes(app: FastifyInstance) {
   app.get(
     '/requests',
     { onRequest: [verifyJWT, verifyUserProfile('Company')] },
     requests,
+  )
+
+  //* Solicitar frete específico disponível (Somente entregadores) */
+  app.post(
+    '/request/:freight_id',
+    {
+      onRequest: [verifyJWT, verifyUserProfile('Deliverer')],
+    },
+    registerRequest,
+  )
+
+  //* Atualizações de frete */
+  //* Aceitar solicitação */
+  app.patch(
+    '/requests/:request_id/accept',
+    { onRequest: [verifyJWT, verifyUserProfile('Company')] },
+    acceptDeliveryRequest,
+  )
+  //* Rejeitar solicitação */
+  app.patch(
+    '/requests/:request_id/decline',
+    { onRequest: [verifyJWT, verifyUserProfile('Company')] },
+    declineDeliveryRequest,
   )
 }
